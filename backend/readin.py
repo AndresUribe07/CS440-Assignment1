28aa696f6de7d465c806bb24f405294f1d20a4ec
Finn Jensen <fjensen21@outlook.com>
2022-01-28 21:36:22 -0500

    - updates to readin file so it is functional up to the point of adding all necessary vertices

--- backend/readin.py
+++ backend/readin.py
@@ -1,15 +1,18 @@
 from graph import Graph
-from vertex import Vertex
 
 
-def generate_vertices(rows: int, cols: int) -> list:
-    vertices = []
-
+def generate_vertices(rows: int, cols: int, g: Graph) -> list:
+    """
+    Given a number of cell rows and columns, generates the appropriate amount of
+    vertices. Note: if there are 4x3 cells there will be 5x4 vertices
+    @param rows:
+    @param cols:
+    @param g:
+    @return:
+    """
     for i in range(1, rows + 2):
         for j in range(1, cols + 2):
-            temp = Vertex(i, j)
-            vertices.append(temp)
-    return vertices
+            g.addVertex(str(i) + str(j))
 
 
 def read_in_graph(filename: str):
@@ -34,10 +37,10 @@ def read_in_graph(filename: str):
         grid_dimensions = (int(tokens[0]), int(tokens[1]))
 
         # Generate vertices
-        new_vertices = generate_vertices(grid_dimensions[0], grid_dimensions[1])
-        g.add_vertices(new_vertices)
+        new_vertices = generate_vertices(grid_dimensions[0], grid_dimensions[1], g)
 
         # TODO Find a way to generate outside edges programmatically
+        
         # TODO Any benefit in representing vertices as a matrix instead of a list?
 
     f.close()
