fe0fe9f4dc40a828fb7ed75934d567ff811e1daa
Finn Jensen <fjensen21@outlook.com>
2022-02-01 08:09:32 -0500

    - generate tests added (incomplete/nonfunctional)

--- backend/generatetests.py
+++ backend/generatetests.py
@@ -34,6 +34,12 @@ def generate_vertices_dict(r: int, c: int) -> dict:
 
     return vertices
 
+def h(s: tuple, g: tuple):
+    return math.sqrt(2) * min(abs(s[0] - g[0]), abs(s[1] - g[1])) + max(abs(s[0] - g[0]), abs(s[1] - g[1])) - min(
+        abs(s[0] - g[0]), abs(s[1] - g[1]))
+
 
 if __name__ == '__main__':
-    generate_tests(3, 'tests')
+    s = (3, 2)
+    g = (0, 0)
+    print(h(s, g))
