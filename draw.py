0d6d7da9ebdcde58d705cebddaa99b08dd38004b
ljm209 <98412258+ljm209@users.noreply.github.com>
2022-02-02 17:50:54 -0500

    Add files via upload
    -"draw.py" has methods for drawing points and lines.  The "lines" function there has a parameter for fill color specifically
    -"fronttest.py" is what I've been using to test this so far
    -"griddisplay.py" is what has the Display class + the ability to display on canvas
    
    So far, no specific method for blocked cells, and nothing for a specific path (the lines function can be called for that, of course)

---
+++ draw.py
@@ -0,0 +1,23 @@
+def points(x, y, canvas_name, vertex_name):
+#Given coordinates and the canvas name, it draws (and tags) the vertices
+    r = 5 #radius—can be changed if the circles need to be bigger
+    x0 = x - r
+    y0 = y - r
+    x1 = x + r
+    y1 = y + r
+    return canvas_name.create_oval(x0, y0, x1, y1, fill="black", tag="vertex")
+    #the tag could be changed to be a specific vertex name if needed, probably unnecessary
+
+
+def lines(x1, y1, x2, y2, canvas_name, fill):
+#Given 4 coordinates (for 2 points), draws a line between them. Also passed canvas name and a string for line color
+    canvas_name.create_line(x1, y1, x2, y2, fill=fill)
+    return
+
+
+def points_and_lines(x1, y1, x2, y2, canvas_name, vertex_name):
+#Given 2 vertices and a canvas name, draws all points and lines between the two vertices
+    lines(x1, y1, x2, y2, canvas_name, "black")
+    points(x1, y1, canvas_name, vertex_name)
+    points(x2, y2, canvas_name, vertex_name)
+    return
---
+++ griddisplay.py
@@ -0,0 +1,62 @@
+import tkinter
+from tkinter import *
+from graph import *
+from vertex import Vertex
+from draw import *
+
+
+class Display:
+
+    root = Tk()
+    root.title = ("Click on vertices for info")
+    canvas_name = Canvas(root, width=1000, height=1000) #dimenisons of canvas should be sized better than this, will look @ later
+    canvas_name.configure(bg="white")
+    canvas_name.pack(fill="x", expand=True)
+
+    def __init__(self, graph):
+    #given graph object, sort of does canvas and root on its own
+        self.graph = graph
+
+    def coordinate_producer(self):
+    #calls methods to draw
+        graph = self.graph
+        canvas_name = self.canvas_name
+
+        for vertex in graph: #goes through vertices
+
+            x, y = Vertex.getKeyCoordinates(vertex)
+            current_neighbors = vertex.getNeighbors()
+
+            for neighbor in current_neighbors:  #goes through a vertex's neighbors
+            #PROBLEM: it will re-draw stuff
+                x_neigh, y_neigh = Vertex.getKeyCoordinates(neighbor)
+                points_and_lines(x, y, x_neigh, y_neigh, canvas_name, vertex)
+
+        return
+
+
+    def display_info(self, event):
+    #Uses the mouse click location to determine which vertex + print appropriate info
+        x = event.x
+        y = event.y
+        info = ""
+
+        for vertex in self.graph:
+            vertex_x, vertex_y = Vertex.getKeyCoordinates(vertex)
+            radius = 5 #must be the same as the radius used in the points method under the draw file; that's how big it appears on screen
+
+            if vertex_x in range(x - radius, x + radius) and vertex_y in range(y - radius, y + radius):
+            #if clicked coordinates are in the drawn vertex's range
+                info = "vertex g: " + str(vertex.g) + " vertex h: " + str(vertex.h)
+                break
+            else: #it's not going to reach this part
+                info = "click on vertex, not line or blank space"
+
+        print(info)
+        return
+
+
+    def bind_canvas(self):
+    #<Button-1> is for mouse click, "vertex" is the tag of all vertices, display_info is what's run when vertex-tagged object is clicked
+        self.canvas_name.tag_bind("vertex", "<Button-1>", self.display_info)
+        self.root.mainloop() #displaying the stuff on the canvas
