e4b3aabd621df0138b6f0b3ad8da0154489b1d97
ljm209 <98412258+ljm209@users.noreply.github.com>
2022-02-04 17:01:44 -0500

    Update draw.py
    -changed the points method so that it can be used more generally (including for start and goal vertex business)

--- draw.py
+++ draw.py
@@ -1,11 +1,11 @@
-def points(x, y, canvas_name, vertex_name):
+def points(x, y, canvas_name, tag, fill, radius):
 #Given coordinates and the canvas name, it draws (and tags) the vertices
-    r = 5 #radius—can be changed if the circles need to be bigger
+    r = radius #radius—can be changed if the circles need to be bigger
     x0 = x - r
     y0 = y - r
     x1 = x + r
     y1 = y + r
-    return canvas_name.create_oval(x0, y0, x1, y1, fill="black", tag="vertex")
+    return canvas_name.create_oval(x0, y0, x1, y1, fill=fill, tag=tag)
     #the tag could be changed to be a specific vertex name if needed, probably unnecessary
 
 
@@ -15,18 +15,9 @@ def lines(x1, y1, x2, y2, canvas_name, fill):
     return
 
 
-def circle(x, y, canvas_name): #for indicating if a vertex is a start or goal node
-    r = 8
-    x0 = x -r
-    y0 = y - r
-    x1 = x + r
-    y1 = y + r
-    return canvas_name.create_oval(x0, y0, x1, y1, fill="red")
-
-
-def points_and_lines(x1, y1, x2, y2, canvas_name, vertex_name):
+def points_and_lines(x1, y1, x2, y2, canvas_name):
 #Given 2 vertices and a canvas name, draws all points and lines between the two vertices
     lines(x1, y1, x2, y2, canvas_name, "black")
-    points(x1, y1, canvas_name, vertex_name)
-    points(x2, y2, canvas_name, vertex_name)
+    points(x1, y1, canvas_name, "vertex", "black", 5)
+    points(x2, y2, canvas_name, "vertex", "black", 5)
     return
