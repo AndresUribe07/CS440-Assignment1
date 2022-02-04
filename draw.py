ec023e86cf5fdc26f3644c86926f60ef1498762c
ljm209 <98412258+ljm209@users.noreply.github.com>
2022-02-04 16:58:20 -0500

    Update draw.py
    -added the "circle" method which is used in the very specific case of start and goal vertices by griddisplay...

--- draw.py
+++ draw.py
@@ -15,6 +15,15 @@ def lines(x1, y1, x2, y2, canvas_name, fill):
     return
 
 
+def circle(x, y, canvas_name): #for indicating if a vertex is a start or goal node
+    r = 8
+    x0 = x -r
+    y0 = y - r
+    x1 = x + r
+    y1 = y + r
+    return canvas_name.create_oval(x0, y0, x1, y1, fill="red")
+
+
 def points_and_lines(x1, y1, x2, y2, canvas_name, vertex_name):
 #Given 2 vertices and a canvas name, draws all points and lines between the two vertices
     lines(x1, y1, x2, y2, canvas_name, "black")
